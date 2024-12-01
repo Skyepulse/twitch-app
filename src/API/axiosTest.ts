3b3608b41519e72508c90c034adab42dd92d5266
Elmrys <elmrys@gmail.com>
2024-12-02 00:03:59 +0100

    StartingOver

--- src/API/axiosTest.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const axiosTest = async () => axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com",
-    timeout: 1000,
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
