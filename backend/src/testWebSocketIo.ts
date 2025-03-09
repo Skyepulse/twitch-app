5347e5afdc843d075d1951f46aa6ad8c42ebe836
Elmrys <elmrys@gmail.com>
2025-03-09 12:40:36 +0100

    modified localhost listening

--- backend/src/testWebSocketIo.ts
+++ backend/src/testWebSocketIo.ts
@@ -36,6 +36,6 @@ app.get("/", (req, res) => {
 
 // Start the server (Only inside the VM)
 const PORT = Number(process.env.PORT || 5000);
-server.listen(PORT, "0.0.0.0", () => {
-    console.log(`🚀 Socket.IO Server running at http://0.0.0.0:${PORT}`);
+server.listen(PORT, "127.0.0.1", () => {
+    console.log(`🚀 Socket.IO Server running at http://127.0.0.1:${PORT}`);
 });
